8c5b4ef67f227a9ac29297f6a0de2c7e7169e2c8
Paul Farrell <87060717+paulbfarrell007@users.noreply.github.com>
2022-08-21 13:31:02 -0400

    8-21

---
+++ Week2/boxes-made-with-props/src/App.js
@@ -0,0 +1,18 @@
+import Card from "./Card.js"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      < Card 
+      title="Title 1" 
+      subtitle="Sub Title 1" 
+      description="Hello World 1"
+      bgcolor="#ED2938"
+      width="200px"
+    />
+    </div>
+  );
+}
+
+export default App;
