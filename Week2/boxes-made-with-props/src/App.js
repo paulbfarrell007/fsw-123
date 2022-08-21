183ff926187b9f97b3133420f14b89674ef1ec07
Paul Farrell <87060717+paulbfarrell007@users.noreply.github.com>
2022-08-21 19:17:05 -0400

    week2

--- Week2/boxes-made-with-props/src/App.js
+++ Week2/boxes-made-with-props/src/App.js
@@ -7,11 +7,35 @@ function App() {
       < Card 
       title="Title 1" 
       subtitle="Sub Title 1" 
-      description="Hello World 1"
+      description="Hello World 1 !"
       bgcolor="#ED2938"
-      width="200px"
+      width="200px"    
+    />
+     < Card 
+      title="Title 2" 
+      subtitle="Sub Title 2" 
+      description="Hello World 2 !"
+      bgcolor="#FEAE0D"
+      width="200px"    
+    />
+
+    < Card 
+      title="Title 3" 
+      subtitle="Sub Title 3" 
+      description="Hello World 3 !"
+      bgcolor="#FEEE0D"
+      width="200px"    
+    />
+
+< Card 
+      title="Title 4" 
+      subtitle="Sub Title 4" 
+      description="Hello World 4 !"
+      bgcolor="#0C8628"
+      width="200px"    
     />
     </div>
+    
   );
 }
 
