f49e00b1aab2d69b45c70a5e1e3e5937da10a1b5
Paul Farrell <87060717+paulbfarrell007@users.noreply.github.com>
2022-08-21 16:00:13 -0400

    Add files via upload

---
+++ week2/todolist/App.js
@@ -0,0 +1,13 @@
+import List from './List';
+import {todos} from './Store';
+import './App.css';
+function App() {
+  return (
+    <div>
+    <h1>List of Todos</h1>
+    <h3><ul><List tasks = {todos} /></ul></h3>
+    </div>
+  );
+}
+
+export default App;
---
+++ week2/todolist/List.js
@@ -0,0 +1,8 @@
+import {todos} from "./Store";
+function List({tasks}) { const list = todos.map((task, index) => {
+return(<div key={index}>
+<input type="checkbox" id={index} name="item"></input>
+<label> {task.text}</label></div>)
+     });
+    return(<>{list}</>)}
+ export default List
