40389e8bc7a18028fbf0de04d2474db8b52f3367
Paul Farrell <87060717+paulbfarrell007@users.noreply.github.com>
2022-09-21 20:54:32 -0400

    Week 3 and 4 and 5 update

--- Week5/giphy/src/App.js
+++ Week5/giphy/src/App.js
@@ -1,18 +1,50 @@
-//// api.giphy.com/v1/gifs/search?api_key=rFOHLC1B46pPfSRGBzFIusBcEFE55NHk&q=music&limit=1
+//// 
+//   console.log('hello')
+  
 
 import './App.css';
-
+import { useEffect, useState } from 'react'; 
+const apiKey = "rFOHLC1B46pPfSRGBzFIusBcEFE55NHk"
 function App() {
-  console.log('hello')
+  const [imgSrc, setImgSrc] = useState("");
+  const [alt, setAlt] = useState("");
+  const [inputValue, setInputValue] = useState("")
   
-const url = "api.giphy.com/v1/gifs/search";
-fetch(
-  "https://api.giphy.com/v1/gifs/search"
+  const getImg=(queries)=>{
+
+    fetch(
+      `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${queries}&limit=1`
+      )
+      .then((response) => response.json())
+      .then(json => {
+        setImgSrc (json.data[0].images.downsized_medium.url)
+        setAlt (json.data.title)
+      })  
+  };    
+  useEffect(() => {  
+  getImg ("music")  
+  }, []
   )
-  .then(response => response.json( ))
-  .then(json => console.log(response.json( ))
+const handleChange = (e) => {
+  setInputValue(e.target.value)
+}
+const handleSubmit = (e) => {
+  getImg (inputValue)
+}
   return (
-     <div> 
+     <div>
+      <div>
+        <input
+        onChange={handleChange}
+        value={inputValue}
+        name="query"
+        placeholder="text goes here"
+        ></input> 
+        <button
+        onClick = {handleSubmit}
+        >search</button>
+      </div>
+  <img src={imgSrc} alt={alt} />
     </div>
   );
 }
